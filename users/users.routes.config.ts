3a197e505d93bb4ab850a3b0a0dd50651fc633cb
Madhur Toppo <madhur.toppo@gmail.com>
2023-08-09 15:14:59 +0530

    Add validations for post, put and patch bodies

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { CommonRoutesConfig } from "../common/common.routes.config";
+import {CommonRoutesConfig} from "../common/common.routes.config";
 import UsersController from "./controllers/users.controller";
 import UsersMiddleware from "./middleware/users.middleware";
+import {body} from 'express-validator';
+import BodyValidationMiddleware from "../common/middleware/body.validation.middleware";
 
 export class UsersRoutes extends CommonRoutesConfig {
   constructor(app: express.Application) {
@@ -13,7 +15,9 @@ export class UsersRoutes extends CommonRoutesConfig {
       .route(`/users`)
       .get(UsersController.listUsers)
       .post(
-        UsersMiddleware.validateRequiredUserBodyFields,
+        body('email').isEmail(),
+        body('password').isLength({min: 5}).withMessage('Must include password (5+ characters)'),
+        BodyValidationMiddleware.verifyBodyFieldsErrors,
         UsersMiddleware.validateSameEmailDoesntExist,
         UsersController.createUser,
       );
@@ -26,12 +30,23 @@ export class UsersRoutes extends CommonRoutesConfig {
       .delete(UsersController.removeUser);
 
     this.app.put(`/users/:userId`, [
-      UsersMiddleware.validateRequiredUserBodyFields,
+      body('email').isEmail(),
+      body('password').isLength({min: 5}).withMessage('Must include password (5+ characters)'),
+      body('firstName').isString(),
+      body('lastName').isString(),
+      body('permissionFlags').isInt(),
+      BodyValidationMiddleware.verifyBodyFieldsErrors,
       UsersMiddleware.validateSameEmailBelongToSameUser,
       UsersController.put,
     ]);
 
     this.app.patch(`/users/:userId`, [
+      body('email').isEmail().optional(),
+      body('password').isLength({min: 5}).withMessage('Password must be 5+ characters').optional(),
+      body('firstName').isString().optional(),
+      body('lastName').isString().optional(),
+      body('permissionFlags').isInt().optional(),
+      BodyValidationMiddleware.verifyBodyFieldsErrors,
       UsersMiddleware.validatePatchEmail,
       UsersController.patch,
     ]);
